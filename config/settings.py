10be6ed6cc7f5450ba1b7300d7fef20eda60fdbd
ShaunR23 <shaunradford23@gmail.com>
2022-02-16 11:22:38 -0500

    heroku settings updated

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    
     'frontend.apps.FrontendConfig',
     'rest_framework',
     'items.apps.ItemsConfig'
